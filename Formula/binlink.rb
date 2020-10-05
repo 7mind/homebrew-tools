ca651cf8397a9039fed178f4943ce859c128cef5
Paul S <pshirshov@gmail.com>
2020-10-06 00:20:50 +0100

    binlink 0.0.5

--- Formula/binlink.rb
+++ Formula/binlink.rb
@@ -1,8 +1,8 @@
 class Binlink < Formula
   desc "Proper shell-free alternative for SDKMAN/jenv/jabba/etc"
   homepage "https://github.com/7mind/binlink"
-  url "https://github.com/7mind/binlink/releases/download/0.0.4/binlink-macos-amd64.zip"
-  sha256 "53f315b753a3dec5814a6a35594bfc2b0db33d7c944d3217bb70e82d7755c387"
+  url "https://github.com/7mind/binlink/releases/download/0.0.5/binlink-macos-amd64.zip"
+  sha256 "e5d6efb8cf5033c7b3587874d8c9c2ee788fd3d1c9fd4a3d4fdbfda16c6384c0"
   license "BSD-3-Clause"
 
   def install
